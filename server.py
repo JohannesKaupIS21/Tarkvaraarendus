33f36b5e3cac8e09b1091c6840a70b2dd7e9a8a3
JohannesKaup_IS21 <95479345+JohannesKaupIS21@users.noreply.github.com>
2022-06-17 08:13:42 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,28 +1,32 @@
-import socket, sys, time # Imprdib need moodulid
+import socket
+import sys
+import time
 
+## end of imports ###
 
-s=socket.socket() # Loob socket objekti
-host=socket.gethostname() # Hosti nimi
-port=8080 # Port
-s.bind((host,port)) # Bindi host ja port
-print('') # prindib
-print('Hostname: '+host) # Prindib hosti nime
-print('') # prindib
-print('[+] Server started on port ' + str(port)) # Prindib serveri porti
-print('')  # prindib
-print('server is waiting for connection...') # Prindib serveri ootamise teade
-print('')  # prindib
-s.listen(1) # Loob serveri kuulamise
-conn, addr = s.accept() # Accepti klienti
-print('[+] Got connection from ' + str(addr)) # Prindib kliendi aadressi
-print('')  # prindib
-while 1: # Loob serveri kuulamise
-    message = input(str('>> ')) # Küsib klientilt sõnumit
-    message = message.encode() # Kodeerib sõnum
-    conn.send(message) # Saadab kliendile sõnumit
-    print('[+] Message sent') # Prindib sõnumi saatmise teate
-    print('')  # prindib
-    incoming_message = conn.recv(1024) # Võtab kliendilt sõnumit
-    incoming_message = incoming_message.decode() # Dekodeerib sõnum
-    print('[+] Message received: ' + incoming_message) # Prindib sõnumi
-    print('')  # prindib
+### init ###
+
+s = socket.socket()
+host = socket.gethostname()
+print(" server will start on host : ", host)
+port = 8080
+s.bind((host,port))
+print("")
+print(" Server done binding to host and port successfully")
+print("")
+print("Server is waiting for incoming connections")
+print("")
+s.listen(1)
+conn, addr = s.accept()
+print(addr, " Has connected to the server and is now online ...")
+print("")
+while 1:
+            message = input(str(">> "))
+            message = message.encode()
+            conn.send(message)
+            print("message has been sent...")
+            print("")
+            incoming_message = conn.recv(1024)
+            incoming_message = incoming_message.decode()
+            print(" Client : ", incoming_message)
+            print("")
